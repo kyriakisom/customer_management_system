29561cf69dfd3cbeefd8001f4311b2d1a8f6fb60
Kyriaki Somatopoulou <147051749+kyriakisom@users.noreply.github.com>
2025-06-16 05:43:30 +0300

    Update User.cs

--- customer_management/CustomerApi/Models/User.cs
+++ customer_management/CustomerApi/Models/User.cs
@@ -4,22 +4,25 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CustomerApi.Models;
 
+// Represents a User entity stored in MongoDB
 public class User
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
+    [BsonId] // Marks this property as the primary key in MongoDB
+    [BsonRepresentation(BsonType.ObjectId)] 
+    // Store the Id as a MongoDB ObjectId (hex string)
     public string Id { get; set; } = string.Empty;
 
-    [Required]
-    [EmailAddress]
+    [Required] // Email is required
+    [EmailAddress] // Must be a valid email format
     public string Email { get; set; } = string.Empty;
 
-    [Required]
+    [Required] // Username is required
     public string Username { get; set; } = string.Empty;
 
-    [Required]
+    [Required] // FullName is required
     public string FullName { get; set; } = string.Empty;
 
-    [Required]
+    [Required] // PasswordHash is required
+    // Stores the hashed password, not the plain text password for security
     public string PasswordHash { get; set; } = string.Empty;
 }
