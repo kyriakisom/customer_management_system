93e885eab85e8b598a73c12abf7f8a3786dc9944
Kyriaki Somatopoulou <147051749+kyriakisom@users.noreply.github.com>
2025-06-16 05:42:10 +0300

    Update AuthDtos.cs

--- customer_management/CustomerApi/Models/AuthDtos.cs
+++ customer_management/CustomerApi/Models/AuthDtos.cs
@@ -2,34 +2,37 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CustomerApi.Models;
 
+// Data Transfer Object for user signup/registration
 public class SignupDto
 {
-    [Required]
-    [EmailAddress]
+    [Required] // Email is required
+    [EmailAddress] // Must be a valid email format
     public string Email { get; set; } = string.Empty;
 
-    [Required]
+    [Required] // Username is required
     public string Username { get; set; } = string.Empty;
 
-    [Required]
+    [Required] // Full name is required
     public string FullName { get; set; } = string.Empty;
 
-    [Required]
-    [DataType(DataType.Password)]
+    [Required] // Password is required
+    [DataType(DataType.Password)] // Treated as a password field (e.g. hides input in UI)
     public string Password { get; set; } = string.Empty;
 
-    [Required]
-    [DataType(DataType.Password)]
-    [Compare("Password", ErrorMessage = "Passwords do not match")]
+    [Required] // Confirm password is required
+    [DataType(DataType.Password)] // Treated as a password field
+    [Compare("Password", ErrorMessage = "Passwords do not match")] 
+    // Must match the value of the Password property; custom error message if not
     public string ConfirmPassword { get; set; } = string.Empty;
 }
 
+// Data Transfer Object for user login
 public class LoginDto
 {
-    [Required]
+    [Required] // Username is required
     public string Username { get; set; } = string.Empty;
 
-    [Required]
-    [DataType(DataType.Password)]
+    [Required] // Password is required
+    [DataType(DataType.Password)] // Treated as a password field
     public string Password { get; set; } = string.Empty;
 }
